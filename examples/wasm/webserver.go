482ae656cb4a8faead719b8a4e8b61faeff77ac0
Andrew Kroh <andrew.kroh@elastic.co>
2022-05-26 10:19:47 -0400

    Add error handling to wasm demo server

--- examples/wasm/webserver.go
+++ examples/wasm/webserver.go
@@ -5,10 +5,13 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
-	fmt.Println("Visit http://localhost:8080")
-	http.ListenAndServe(`localhost:8080`, http.FileServer(http.Dir(`.`)))
+	fmt.Println("Visit http://localhost:8081")
+	if err := http.ListenAndServe(`localhost:8081`, http.FileServer(http.Dir(`.`))); err != nil {
+		log.Fatal("ERROR:", err)
+	}
 }
